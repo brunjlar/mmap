1594bbf611a29062c2879dd5247566807b04a409
Lars Brünjes <brunjlar@gmail.com>
2019-08-20 09:48:44 +0200

    initial commit

---
+++ src/Data/Map/Monoidal.hs
@@ -0,0 +1,7 @@
+module Data.Map.Monoidal
+    (
+    ) where
+
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import           Optics.Lens
---
+++ src/Data/Set/NonEmpty.hs
@@ -0,0 +1,3 @@
+module Data.Set.NonEmpty
+    (
+    ) where
